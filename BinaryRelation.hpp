a71154ca35b0d7fa8a926f77cc8c56eec5340756
Martin Dimitrov <donmartineze3@gmail.com>
2020-06-01 15:42:22 +0300

    working prototype added overloaded for: operator () operator ! operator =

--- BinaryRelation.hpp
+++ BinaryRelation.hpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#define DEFAULT_INITIAL_CAPACITY 10
+
 template<typename T, typename U>
 class BinaryRelation
 {
@@ -14,11 +16,16 @@ public:
 
     BinaryRelation &BinaryRelation::operator=(const BinaryRelation<T, U> &other);
 
+    BinaryRelation &operator!();
+
+    bool &operator()(const T &first, const U &second);
+
     ~BinaryRelation();
 
     void addRelation(const T &newFirst, const U &newSecond);
 
     void printAllRelations();
+
 private:
     T *fRelationsFirst;
     U *fRelationsSecond;
@@ -39,7 +46,10 @@ private:
 template<typename T, typename U>
 BinaryRelation<T, U>::BinaryRelation()
 {
-    BinaryRelation<T, U>(10);
+    fCapacity = DEFAULT_INITIAL_CAPACITY;
+    fSize = 0;
+    fRelationsFirst = new T[fCapacity];
+    fRelationsSecond = new U[fCapacity];
 }
 
 template<typename T, typename U>
@@ -73,8 +83,14 @@ BinaryRelation<T, U>::BinaryRelation(const BinaryRelation<T, U> &other)
 template<typename T, typename U>
 void BinaryRelation<T, U>::copy(const BinaryRelation<T, U> &other)
 {
+    fRelationsFirst = nullptr;
+    fRelationsSecond = nullptr;
+
     fRelationsFirst = new T[other.fCapacity];
     fRelationsSecond = new U[other.fCapacity];
+
+    fCapacity = other.fCapacity;
+    fSize = other.fSize;
 
     for (int i = 0; i < other.fSize; i++)
     {
@@ -91,10 +107,8 @@ BinaryRelation<T, U> &BinaryRelation<T, U>::operator=(const BinaryRelation<T, U>
         delMem();
         copy(other);
     }
-    else
-    {
-        return *this;
-    }
+
+    return *this;
 }
 
 template<typename T, typename U>
@@ -134,4 +148,37 @@ void BinaryRelation<T, U>::printAllRelations()
         std::cout << fRelationsFirst[i] << " " << fRelationsSecond[i] << "\n";
     }
 
+}
+
+template<typename T, typename U>
+bool &BinaryRelation<T, U>::operator()(const T &first, const U &second)
+{
+    bool inRelation = false;
+
+    for (int i = 0; i < fSize; i++)
+    {
+        if (fRelationsFirst[i] == first && fRelationsSecond[i] == second)
+        {
+            inRelation = true;
+            break;
+        }
+    }
+
+    return inRelation;
+}
+
+template<typename T, typename U>
+BinaryRelation<T, U> &BinaryRelation<T, U>::operator!()
+{
+    BinaryRelation<T, U> *tempRelation = new BinaryRelation<T, U>(fCapacity);
+    tempRelation->fCapacity = fCapacity;
+    tempRelation->fSize = fSize;
+
+    for (int i = 0; i < fSize; i++)
+    {
+        tempRelation->fRelationsFirst[i] = fRelationsSecond[i];
+        tempRelation->fRelationsSecond[i] = fRelationsFirst[i];
+    }
+
+    return *tempRelation;
 }
--- main.cpp
+++ main.cpp
@@ -4,12 +4,31 @@
 int main()
 {
     BinaryRelation<int,int> test1;
+    BinaryRelation<int,int> test2;
+    BinaryRelation<int,char const*> test3;
+
+    test3.addRelation(1, "one");
+    test3.addRelation(2, "two");
+    test3.addRelation(3, "three");
 
 
     test1.addRelation(1,11);
     test1.addRelation(2,22);
 
+    test2 = test1;
+
     test1.printAllRelations();
+    std::cout << "\n\n\n";
+
+    test2 = !test1;
+
+    test2.printAllRelations();
+    std::cout << "\n\n\n";
+
+    test3.printAllRelations();
+    //std::cout << test2(1,21);
+
+
 
 
     return 0;
